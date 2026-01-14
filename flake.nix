bc66cb936d22e07576905cf5c0c8014561ebddec
Vadim Storozhiloff <yanechearer@gmail.com>
2026-01-14 08:19:58 +0500

    minimal nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Minimal neofetch-like weather CLI";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      naersk,
+    }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      naerskLib = pkgs.callPackage naersk { };
+    in
+    {
+      packages.x86_64-linux.default = naerskLib.buildPackage {
+        src = ./.;
+        buildInputs = [ pkgs.glibc ];
+        nativeBuildInputs = [ pkgs.pkg-config ];
+      };
+    };
+}
